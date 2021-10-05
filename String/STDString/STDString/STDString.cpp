7a2809eeea95da63c08e6479f8c9a40192e30919
fanjaekim <hjjang45@gmail.com>
2021-10-05 23:37:45 +0900

    C++ String 클래스 예제1

---
+++ String/STDString/STDString/STDString.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(void)
+{
+	string str1 = "I like ";
+	string str2 = "string class";
+	string str3 = str1 + str2; // 연산자 오버로딩으로 인해 string 객체간 덧셈 연산이 가능하다.
+
+	cout << str1 << endl; // string 클래스는 << 연산자에 대해서도 오버로딩 되어있음.
+	cout << str2 << endl;
+	cout << str3 << endl;
+
+	str1 += str2; 
+	if (str1 == str3)
+	{
+		cout << "동일 문자열!" << "\n";
+	}
+	else
+	{
+		cout << "동일하지 않은 문자열!" << "\n";
+	}
+
+	string str4;
+	cout << "문자열 입력 : ";
+	cin >> str4; // string 클래스는 >> 연산자에 대해서도 오버로딩 되어있음.
+	cout << "입력한 문자열: " << str4 << "\n";
+	return 0;
+}
