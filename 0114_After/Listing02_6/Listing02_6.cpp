db62ec4989defd55806aeb3d4d3409fa986f6cba
fanjaekim <hjjang45@gmail.com>
2022-01-15 16:36:09 +0900

    C++ 기초 플러스 예제 2.6

---
+++ 0114_After/Listing02_6/Listing02_6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+int stonetolib(int); // 함수 원형
+int main()
+{
+	using namespace std;
+	int stone;
+	cout << "체중을 스톤 단위로 입력하시오: ";
+	cin >> stone;
+	int pounds = stonetolib(stone);
+	cout << stone << " 스톤은 ";
+	cout << pounds << " 파운드입니다." << endl;
+	return 0;
+}
+
+int stonetolib(int sts)
+{
+	return 14 * sts;
+}
