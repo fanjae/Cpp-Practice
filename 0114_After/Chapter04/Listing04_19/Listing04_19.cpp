428714ede80e5eaacf905aca3ee7c540d16e8201
fanjaekim <hjjang45@gmail.com>
2022-01-19 21:44:32 +0900

    C++ 기초 플러스 6판 Chapter 4 - 포인터와 문자열 파트

---
+++ 0114_After/Chapter04/Listing04_19/Listing04_19.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+int main()
+{
+	using namespace std;
+	double wages[3] = { 10000.0, 20000.0, 30000.0 };
+	short stacks[3] = { 3,2,1 };
+
+	// 배열의 주소를 알아내는 두 가지 방법
+
+	double *pw = wages;					// 배열 이름 = 주소
+	short *ps = &stacks[0];				// 배열 원소에 주소 연산자 사용
+
+	cout << "pw = " << pw << ", *pw = " << *pw << endl;
+	pw = pw + 1;
+	cout << "pw 포인터에 1을 더함:\n";
+	cout << "pw = " << pw << ", *pw = " << *pw << "\n\n";
+
+	/* 포인터 변수에 1을 더하면 값이 그 포인터가 지시하는
+	데이터 형의 바이트 수만큼 증가한다. */
+
+	cout << "ps = " << ps << ", *ps = " << *ps << endl;
+	ps = ps + 1;
+	cout << "ps 포인터에 1을 더함:\n";
+	cout << "ps = " << ps << ", *ps = " << *ps << "\n\n";
+
+	cout << "배열 표기로 두 원소에 접근\n";
+	cout << "stacks[0] = " << stacks[0] << ", stacks[1] = " << stacks[1] << endl;
+
+	cout << "포인터 표기로 두 원수에 접근\n";
+	cout << "*stacks = " << *stacks << ", *(stacks + 1) = " << *(stacks + 1) << endl;
+
+	cout << sizeof(wages) << " = wages 배열의 크기\n";
+	cout << sizeof(pw) << " = pw 포인터의 크기\n";
+	return 0;
+}
---
+++ 0114_After/Chapter04/Listing04_20/Listing04_20.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <cstring>				// strlen(), strcpy() 선언
+
+#pragma warning (disable:4996)
+int main()
+{
+	using namespace std;
+	char animal[20] = "bear";	// animal에 bear에 들어 있다.
+	const char *bird = "wren";	// bird에 문자열의 주소가 들어가 있다.
+	char *ps;					// 초기화되지 않았다.
+
+	cout << animal << "and ";	// bear를 출력한다.
+	cout << bird << "\n";		// wren을 출력한다.
+	// cout << ps << "\n";		// 쓰레기를 출력하거나 먹통이 될 수도 있다.
+
+
+	cout << "동물의 종류를 입력하십시오: ";
+	cin >> animal;
+	// cin >> ps; (절대 해서는 안됨)
+	// ps가 대입된 공간을 지시하고 있지 있다.
+
+	ps = animal;				// ps를 문자열을 지시하도록 설정한다.
+	cout << ps << "s!\n";		// 맞다, animal을 사용하는 것과 동등 
+	cout << "strcpy() 사용 전:\n";
+	cout << (int *)animal << ": " << animal << endl;
+	cout << (int *)ps << ": " << ps << endl;
+
+	/*
+	ps는 "fox"를 출력하지만 (int *) ps는 그 문자열이 저장된 주소를 출력한다.
+	ps에 ainimal을 대입하면 문자열이 아닌 주소가 복사되므로, 두 포인터는 같은 메모리 위치와 같은 문자열을 지시한다.
+	*/
+
+	ps = new char[strlen(animal) + 1];		// 새 메모리를 대입한다.
+	strcpy(ps, animal);						// 새 메모리에 문자열을 복사
+	cout << "strcpy() 사용 후:\n";
+	cout << (int *)animal << ": " << animal << endl;
+	cout << (int *)ps << ": " << ps << endl;
+	delete[] ps;
+	return 0;
+}
