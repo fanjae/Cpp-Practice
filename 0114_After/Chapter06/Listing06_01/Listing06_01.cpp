a8a071e3d3ef9787caff0f0cb5f0e01572536a13
fanjaekim <hjjang45@gmail.com>
2022-01-25 19:27:13 +0900

    C++ 기초 플러스 Chapter6 예제 - if 구문

---
+++ 0114_After/Chapter06/Listing06_01/Listing06_01.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+int main()
+{
+	using std::cin;
+	using std::cout;
+	char ch;
+
+	int spaces = 0;
+	int total = 0;
+	cin.get(ch);
+	while (ch != '.') // 문장의 끝(마침표)에서 종료.
+	{
+		if (ch == ' ')	// ch가 빈칸인지 검사.
+			++spaces;	// 빈칸이면 수행함.
+		++total;		// 빈칸 여부 상관없이 매번 수행.
+		cin.get(ch);
+	}
+	cout << "이 문장의 총 문자 수는 " << total << "이고, ";
+	cout << "그 중에서 빈칸은 " << spaces << "개입니다.\n";
+	return 0;
+}
