05244c49cd7be5a912d4b18c0165ff05b056e953
fanjaekim <hjjang45@gmail.com>
2022-01-28 16:36:52 +0900

    C++ 기초 플러스 Chapter 6 연습문제 - 분기 구문과 논리 연산자

---
+++ 0114_After/Chapter06_Exercise/Exercise04/Exercise04.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+const int strsize = 100;
+// Benevolent Order of Programmers 회원 구조체
+
+using namespace std;
+struct bop
+{
+	char fullname[strsize];			// 실명
+	char title[strsize];			// 직함
+	char bopname[strsize];			// BOP 아이디
+	int preference;					// 0 = fullname, 1 = title, 2 = bopname
+};
+
+void menu(void);
+
+int main()
+{
+	menu();
+	
+	char ch;
+	bop data[5] = {
+	{"Wimp Macho", "CEO", "Dong",0},
+	{"Raki Rhodes", "Junior Programmer", "Raki Rhodes",0},
+	{"Celia Laiter", "MIPS", "Celia Laiter",1},
+	{"Hopy Hipman", "Analyst Trainee", "Hopy Hipman",1},
+	{"Pat Hand", "MIPS", "LOOPY"} };
+
+	cout << "원하는 것을 선택하십시오: ";
+	cin >> ch;
+	while (ch != 'q')
+	{
+		if (ch == 'a')
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				cout << data[i].fullname << endl;
+			}
+		}
+		else if (ch == 'b')
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				cout << data[i].title << endl;
+			}
+		}
+		else if(ch == 'c')
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				cout << data[i].bopname << endl;
+			}
+		}
+		else if(ch == 'd')
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				switch (data[i].preference)
+				{
+					case 0:
+					{
+						cout << data[i].fullname << endl;
+						break;
+					}
+					case 1:
+					{
+						cout << data[i].title << endl;
+						break;
+					}
+					case 2:
+					{
+						cout << data[i].bopname << endl;
+						break;
+					}
+				}
+			}
+		}
+		else if (ch == 'q')
+		{
+			cout << "프로그램을 종료합니다.\n";
+			break;
+		}
+		cout << "원하는 것을 선택하십시오: ";
+		cin >> ch;
+	}
+}
+
+void menu(void)
+{
+	cout << "a. 실명으로 열람		b. 직함으로 열람\n";
+	cout << "c. BOP 아이디로 열람		d. 회원이 지정한 것으로 열람\n";
+	cout << "q. 종료\n";
+}
+
+
+
---
+++ 0114_After/Chapter06_Exercise/Exercise05/Exercise05.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int tvarp;
+	double tax = 0;
+	cout << "트바프를 입력하시오 : ";
+	cin >> tvarp;
+
+	if (tvarp >= 35000)
+	{
+		tax += (tvarp - 35000) * 0.20;
+		tax += 20000 * 0.15;
+		tax += 10000 * 0.10;
+		tax += 5000 * 0;
+	}
+	else if (tvarp >= 20000)
+	{
+		tax += (tvarp - 20000) * 0.20;
+		tax += 10000 * 0.10;
+		tax += 5000 * 0;
+	}
+	else if (tvarp >= 10000)
+	{
+		tax += (tvarp - 10000) * 0.10;
+		tax += 5000 * 0;
+	}
+	else
+	{
+		tax += (tvarp - 5000) * 0;
+	}
+
+
+	cout << "납부할 세금은 " << tax << " 트바프 입니다.";
+}
