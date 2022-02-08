1e206d03308c96351fbd0aaafdd54147a703e466
fanjaekim <hjjang45@gmail.com>
2022-02-08 23:42:42 +0900

    C++ 기초 플러스 6판 Chapter 8 예제 - 인라인 함수 및 참조 변수

---
+++ 0114_After/Chapter08/Listing08_5/Listing08_5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+double cube(double a);
+double refcube(double &ra);
+int main()
+{
+	using namespace std;
+	double x = 3.0;
+
+	cout << cube(x);
+	cout << " = " << x << "의 세제곱\n";
+	cout << refcube(x);
+	cout << " = " << x << "의 세제곱\n";
+	/* 함수에 전달하는 정보를 그 함수가 변경하지 않고 사용만 하도록 하려면,
+	상수 참조를 사용해야 한다. */
+
+	return 0;
+}
+
+double cube(double a) // 
+{
+	a *= a * a;
+	return a;
+}
+double refcube(double &ra)
+{
+	ra *= ra * ra;
+	return ra;
+}
