1311903bd82bcf8c856cf9d108200c429679bd32
fanjaekim <hjjang45@gmail.com>
2022-02-12 22:20:38 +0900

    C++ Boost.Asio 기초 예제

---
+++ 0114_After/BoostAsio/BoostAsio1/BoostAsio1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace std;
+
+int main(void) {
+	boost::asio::io_service io;
+	boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
+	t.wait();
+
+	cout << "hello world\n";
+	return 0;
+}
+
+/*
+링커->일반->추가 라이브러리 디렉토리
+C:\boost_1_78_0\boost_1_78_0\stage\lib
+
+VC++ 디렉토리 포함 디렉토리
+C:\boost_1_78_0\boost_1_78_0\
+*/
