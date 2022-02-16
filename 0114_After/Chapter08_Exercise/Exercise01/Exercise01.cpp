406ff17001c4769feacd080b14b3366a55121d42
fanjaekim <hjjang45@gmail.com>
2022-02-16 12:34:42 +0900

    C++ 기초 플러스 6판 Chapter 8 연습문제 - 연습문제 1

---
+++ 0114_After/Chapter08_Exercise/Exercise01/Exercise01.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int function_call_count = 0;
+void print(const char *data);
+void print(const char *data, int value);
+int main(void)
+{
+	const char data[100] = { "Username is FanJae." };
+	print(data);
+	print(data);
+	print(data, 1);
+}
+void print(const char *data)
+{
+	cout << data << endl;
+	function_call_count++;
+}
+
+void print(const char *data, int value)
+{
+	if (value == 0)
+	{
+		return;
+	}
+	for (int i = 1; i <= function_call_count; i++)
+	{
+		cout << data << endl;
+	}
+	function_call_count++;
+}
