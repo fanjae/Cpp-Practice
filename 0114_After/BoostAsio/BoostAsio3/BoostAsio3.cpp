0660e06d1bc5851a43ed68aad55e280bc96ac2b4
fanjaekim <hjjang45@gmail.com>
2022-02-16 18:00:33 +0900

    C++ Boost.Asio 연습 - Handler에 대한 Binding 함수 구현 중

---
+++ 0114_After/BoostAsio/BoostAsio3/BoostAsio3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+void print(const boost::system::error_code & /*e*/, boost::asio::steady_timer *t, int * count)
+{
+	if (*count < 5)
+	{
+		std::cout << *count << std::endl;
+		++(*count);
+	}
+	t->expires_at(t->expiry() + boost::asio::chrono::seconds(1));
+}
+
+int main(void)
+{
+
+}
