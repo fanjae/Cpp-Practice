b3c8f33dc52a8a4b774448528ea6d12f402ae300
fanjaekim <hjjang45@gmail.com>
2022-02-16 12:24:09 +0900

    C++ Boost.Asio 연습 - 비동기 타이머 설명 추가

--- 0114_After/BoostAsio/BoostAsio2/BoostAsio2.cpp
+++ 0114_After/BoostAsio/BoostAsio2/BoostAsio2.cpp
@@ -29,7 +29,12 @@ int main()
 	asio 라이브러리는 현재 io_context::run()을 호출한 스레드에서만 콜백 핸들러가 호출되도록 보장한다.
 	따라서 io_context::run() 함수는 비동기 대기 완료를 위한 콜백이 호출되지 않는 한 결코 호출되지 않는다.
 
-	
+	io_context::run() 함수는 work(작업)이 있는 동안 계속 실행된다. 이 예제에서 작업은, 타이머에서 비동기 대기이므로,
+	타이머가 만료되거나 콜백이 완료될때까지 반환도지ㅣ 않는다.
+
+	io_context::run() 호출 전, io_context에 해야할 일을 제공하는 것이 중요.
+	위에서 steady_timer::async_wait()에 대한 호출을 생략한다면 io_context는 할 일이 없었을 것이고,
+	결과적으로 io_context::run()은 즉시 반환할 것이다. */
+ 
 	return 0;
-	
 }
