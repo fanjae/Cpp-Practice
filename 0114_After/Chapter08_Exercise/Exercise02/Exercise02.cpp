559cd7f95e42f7f7b4f1584d450ce125071c5b8c
fanjaekim <hjjang45@gmail.com>
2022-02-17 12:55:14 +0900

    C++ 기초 플러스 6판 Chapter 8 - 연습문제 2번

---
+++ 0114_After/Chapter08_Exercise/Exercise02/Exercise02.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#pragma warning (disable:4996)
+struct CandyBar
+{
+	char name[100];
+	double kg;
+	int kcal;
+};
+
+void Set_CandyBar(CandyBar &value, const char Name[100] = { "Millennium Munch" }, double Kg = 2.85, int Kcal = 350);
+int main()
+{
+	using namespace std;
+
+	CandyBar data;
+	Set_CandyBar(data);
+
+	cout << "상표명 : " << data.name << endl;
+	cout << "중량 : " << data.kg << endl;
+	cout << "칼로리 : " << data.kcal << endl;
+}
+void Set_CandyBar(CandyBar &value, const char Name[100],double Kg,int Kcal)
+{
+	strcpy(value.name, Name);
+	value.kg = Kg;
+	value.kcal = Kcal;
+}
