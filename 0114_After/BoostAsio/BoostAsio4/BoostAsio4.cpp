9f8f4df474a3174229a15ad8a8f0a4477ae31192
fanjaekim <hjjang45@gmail.com>
2022-02-20 23:32:04 +0900

    C++ Boost.Asio 연습 - 핸들러로 멤버 함수를 사용하는 예제

--- 0114_After/BoostAsio/BoostAsio4/BoostAsio4.cpp
+++ 0114_After/BoostAsio/BoostAsio4/BoostAsio4.cpp
@@ -1,20 +1,54 @@
-﻿// BoostAsio4.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
 
-#include <iostream>
+/* BoostAsio3과 동일하게 실행되어야 함. */
+class printer
+{
+public:
+	printer(boost::asio::io_context& io) : timer_(io, boost::asio::chrono::seconds(1)), count_(0)
+	{
+		timer_.async_wait(boost::bind(&printer::print, this));
+	}
+	~printer()
+	{
+		std::cout << "Final count is " << count_ << std::endl;
+	}
+
+	void print()
+	{
+		if (count_ < 5)
+		{
+			std::cout << count_ << std::endl;
+			++count_;
+			timer_.expires_at(timer_.expiry() + boost::asio::chrono::seconds(1));
+			timer_.async_wait(boost::bind(&printer::print, this));
+
+			/* boost::bind() 함수는 사용가능한 함수와 마찬가지로
+			클래스 멤버 함수에서도 잘 작동함.
+			모든 비-정적 클래스 멤버 함수에는 암시적 this 파라미터가 있으므로 이를 함수에 바인딩해야 한다.
+			boost::bind()는 콜백 핸들러(현재의 멤버 함수)를 void(const boost::system::error_code&) 서명이 있는 것처럼
+			호출 될 수 있는 함수 객체로 변환한다. */
+
+			/* 
+			boost::asio::placeholders::error 대체자(placeholder)는 print 멤버 함수에서 파라미터로
+			오류 개체를 허용하지 않기 때문에 여기서 지정되지 않은 점에 유의. */
+		}
+	}
+
+private:
+	boost::asio::steady_timer timer_;
+	int count_;
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	boost::asio::io_context io;
+	printer p(io);
+	io.run();
+
+	return 0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
-
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+/* Chrono : C++11에서 추가된 시간 라이브러리
+chrono는 나노 밀리 초 단위까지 측정이 가능함. */
