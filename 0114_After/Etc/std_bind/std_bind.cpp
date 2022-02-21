a09d659ba841bb04c1d903f158a50e444e832c88
fanjaekim <hjjang45@gmail.com>
2022-02-21 14:32:42 +0900

    C++ 함수 포인터를 확장한 std::function, std::bind - std::function() - std::bind()

---
+++ 0114_After/Etc/std_bind/std_bind.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <functional>
+
+void addAndPrint(int a, int b)
+{
+	int sum = a + b;
+	std::cout << sum << "\n";
+}
+
+int main(void)
+{
+	auto func2 = std::bind(addAndPrint, 10, std::placeholders::_1);
+	// auto -> std::function<void(int)>와 동일함.
+	// std::bind(addAndPrint,10,std::placeholders::_1)의 의미는 addAndPrint(int, int) 함수의
+	// 첫 번째 파라미터는 10, 두 번째 파라미터는 func2를 호출할 때 들어오는 첫번쨰 인자를 넣어준다.
+
+
+	func2(100);
+
+}
---
+++ 0114_After/Etc/std_function/std_function.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <functional>
+
+void addAndPrint(int a, int b)
+{
+	int sum = a + b;
+	std::cout << sum << "\n";
+}
+class A
+{
+public:
+	void addAndPrint(int a, int b)
+	{
+		int sum = a + b;
+		std::cout << "sum : " << sum << "\n";
+	}
+};
+
+A test;
+// class의 멤버함수를 가르키고 싶으면, 멤버함수를 위해서
+// 어떤 낵체의 멤버함수를 실행할 지 알 수 있도록 객체를 전달해야한다.
+std::function<void(int, int)> func1 = addAndPrint;
+std::function<void(A&,int, int)> func2 = &A::addAndPrint;
+
+// 멤버함수의 경우엔 함수의 주소값으로 암시적 형변환이 되지 않아서
+// 명시적으로 &를 사용해서 주소값을 전달해줘야 한다.
+
+int main()
+{
+	addAndPrint(10, 20);
+	func1(10, 20);
+	func3(test, 10, 20);
+}
