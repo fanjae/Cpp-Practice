96819a4e97461053793c503df92e7039452e165f
fanjaekim <hjjang45@gmail.com>
2022-02-24 17:44:33 +0900

    C++ 기초 플러스 6판 Chapter 8 연습문제 3

---
+++ 0114_After/Chapter08_Exercise/Exercise03/Exercise03.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void function_upper(string &data);
+int main(void)
+{
+	string data;
+	while (1)
+	{
+		cout << "문자열을 입력하시오 (끝내려면 q) : ";
+		getline(cin, data);
+
+		if (data[0] == 'q')
+		{
+			cout << "프로그램을 종료합니다." << endl;
+			break;
+		}
+		else
+		{
+			function_upper(data);
+			cout << data << "\n";
+		}
+	}
+}
+void function_upper(string &data)
+{
+	int str_size = data.size();
+	for (int i = 0; i < str_size; i++)
+	{
+		data[i] = toupper(data[i]);
+	}
+}
