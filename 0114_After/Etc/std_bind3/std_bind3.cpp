4fe0d88d3b4e85dc177984174bdac372faaff36e
fanjaekim <hjjang45@gmail.com>
2022-02-24 18:02:03 +0900

    C++ std::bind 연습 - std::placeholders에 대한 추가 확인이 필요할 것으로 보임

--- 0114_After/Etc/std_bind3/std_bind3.cpp
+++ 0114_After/Etc/std_bind3/std_bind3.cpp
@@ -1,14 +1,20 @@
 ﻿#include <iostream>
 #include <functional>
 
-int Calc(int a, int b, int c)
+using namespace std;
+int Calc(int a, int b, int c,int d)
 {
-	return (a + b) * c;
+	return (a + b) * c + d;
 }
+
+/* std::function <int(int, int, int)> 
+std::placeholders로 대체할 매개변수의 수만큼 명시*/
+
+/* std::placeholder에 대해서 추가로 찾을 필요가 존재함 */
 
 int main(void)
 {
-	std::function<int()> data = std::bind(Calc, 5, std::placeholders::_1, std::placeholders::_2);
-	std::cout << data() << std::endl;
+	std::function<int(int, int,int)> data = std::bind(Calc, 5, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+	std::cout << data(1,2,5) << std::endl;
 	return 0;
 }
