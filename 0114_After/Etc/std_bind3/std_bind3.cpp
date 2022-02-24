b879bf8e309c24f9369d4f1a90459d6636b274aa
fanjaekim <hjjang45@gmail.com>
2022-02-24 17:33:34 +0900

    C++ std::bind 연습 - placeholder 사용시 원인 모를 오류 발생 - 원인 찾는중.

---
+++ 0114_After/Etc/std_bind3/std_bind3.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <functional>
+
+int Calc(int a, int b, int c)
+{
+	return (a + b) * c;
+}
+
+int main(void)
+{
+	std::function<int()> data = std::bind(Calc, 5, std::placeholders::_1, std::placeholders::_2);
+	std::cout << data() << std::endl;
+	return 0;
+}
