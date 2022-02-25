39cea20475f2ae7daff78d8f9c284bb6e29db74a
fanjaekim <hjjang45@gmail.com>
2022-02-25 16:06:47 +0900

    C++ 기초 플러스 6판 Chapter 8 연습문제 7

---
+++ 0114_After/Chapter08_Exercise/Exercise07/Exercise07.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+
+template <typename T>
+T ShowArray(T arr[], int n);
+
+template <typename T>
+T ShowArray(T *arr[], int n);
+
+struct debts
+{
+	char name[50];
+	double amount;
+};
+
+int main(void)
+{
+	using namespace std;
+	int things[6] = { 13, 31, 103, 301, 310, 130 };
+	struct debts mr_E[3] =
+	{
+		{"Ima Wolfe", 2400.0},
+		{"Ura Foxe", 1300.0},
+		{"Iby Stout", 1800.0}
+	};
+	double *pd[3];
+	double sum = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		pd[i] = &mr_E[i].amount;
+	}
+
+	cout << "Mr. E의 재산 합계: \n";
+	sum = ShowArray(things, 6);
+	cout << sum << endl;
+
+	sum = 0;
+	cout << "Mr. E의 채무 합계: \n";
+	sum = ShowArray(pd, 3);
+	cout << sum << endl;
+
+	return 0;
+}
+
+template <typename T>
+T ShowArray(T arr[], int n)
+{
+	using namespace std;
+	T sum = 0;
+	cout << "템플릿 A\n";
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+}
+
+template <typename T>
+T ShowArray(T *arr[], int n)
+{
+	using namespace std;
+	T sum = 0;
+	cout << "템플릿 B\n";
+	for (int i = 0; i < n; i++)
+	{
+		sum += *arr[i];
+	}
+	return sum;
+}
