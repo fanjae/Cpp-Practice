b3df4e25e8869d4a184b6b1bb74dbc0d21bf28b6
fanjaekim <hjjang45@gmail.com>
2022-02-25 15:22:04 +0900

    C++ 기초 플러스 6판 Chapter 8 연습문제 5

---
+++ 0114_After/Chapter08_Exercise/Exercise05/Exercise05.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+template <class T>
+T max5(T arr[]);
+
+int main()
+{
+	int int_max_value = 0;
+	int a[5] = { 1,2,3,4,5 };
+
+	double double_max_value = 0;
+	double b[5] = { 1.1,2.2,3.3,4.4,5.5 };
+
+	int_max_value = max5(a);
+	double_max_value = max5(b);
+
+	std::cout << int_max_value << std::endl;
+	std::cout << double_max_value << std::endl;
+
+}
+
+template <class T>
+T max5(T arr[])
+{
+	T max = arr[0];
+	for (int i = 0; i < 5; i++)
+	{
+		if (max < arr[i])
+		{
+			max = arr[i];
+		}
+	}
+	return max;
+}
