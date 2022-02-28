8fd4d2d3a9f26615803a5f8360d8d10d88c9a114
fanjaekim <hjjang45@gmail.com>
2022-02-28 16:11:42 +0900

    C++ 함수 포인터 예제 - 함수 호출 방식 및 정적 함수와 멤버 함수간 함수 포인터 사용 차이

---
+++ 0114_After/Etc/FunctionPointer/FunctionPointer.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+void Print()
+{
+	cout << "정적 함수 Print()" << endl;
+}
+
+class Point
+{
+public:
+	void Print()
+	{
+		cout << "멤버 함수 Print()" << endl;
+	}
+};
+
+int main()
+{
+	Point pt;
+	Point *p = &pt;
+
+	Print();			// 첫째, 정적 함수 호출
+	pt.Print();			// 둘째, 객체로 멤버 함수 호출
+	p->Print();			// 셋째, 주소로 멤버 함수 호출
+	
+	/* C++에서 함수를 호출하는 3가지 방식 */
+	return 0;
+}
