884da896d3b6bd01ad30fa98a3853344992cf105
fanjaekim <hjjang45@gmail.com>
2022-03-01 17:24:43 +0900

    C++ 연습 - Function Obejct

---
+++ 0114_After/Etc/FunctionObject1/FunctionObject1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+void Print()
+{
+	cout << "전역 함수" << endl;
+}
+
+struct Functor
+{
+	void operator()(void)
+	{
+		cout << "함수 객체!" << endl;
+	}
+};
+
+int main()
+{
+	Functor functor;
+
+	Print();	// 전역 함수 호출
+	functor();	// 멤버 함수 호출 functor.operator()와 동일.
+
+	return 0;
+}
---
+++ 0114_After/Etc/FunctionObject3/FunctionObject3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Adder
+{
+	int total;
+public:
+	explicit Adder(int n = 0) : total(n) { }
+	int operator()(int n)
+	{
+		return total += n;
+	}
+};
+
+int main()
+{
+	Adder add(0);
+
+	cout << add(10) << endl; // 10 누적 -> 10
+	cout << add(20) << endl; // 20 누적 -> 20
+	cout << add(30) << endl; // 30 누적 -> 30
+
+	return 0;
+}
