32eea2f9e1f943e97af45632d3a2604a3acab9e5
fanjaekim <hjjang45@gmail.com>
2022-03-06 00:01:32 +0900

    C++ 연습 - 함수 템플릿 특수화가 필요한 이유

---
+++ 0114_After/Etc/FunctionTemplate8/FunctionTemplate8.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point
+{
+	int x;
+	int y;
+public:
+	explicit Point(int _x = 0, int _y = 0) : x(_x), y(_y) { }
+	void Print() const {
+		cout << x << ',' << y << endl;
+	}
+};
+
+template<typename T>
+void Print(T a)
+{
+	cout << a << endl;
+}
+
+int main()
+{
+	int n = 10;
+	double d = 2.5;
+	Point pt(2, 3);
+
+	Print(n);
+	Print(d);
+	Print(pt);	// cout << pt; 연산이 불가능하여 불가능.
+	// 템플릿 특수화가 필요한 이유.
+
+	return 0;
+}
