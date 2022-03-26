4c7153646f31263242b8b8091fc09f12001dbfee
fanjaekim <hjjang45@gmail.com>
2022-03-26 23:33:07 +0900

    C++ 기초 플러스 Chapter 10 - this 포인터 예제

--- 0114_After/Chapter10/stock20/stock20.cpp
+++ 0114_After/Chapter10/stock20/stock20.cpp
@@ -1,20 +1,106 @@
-﻿// stock20.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <iostream>
+#include "stock20.h"
 
-#include <iostream>
-
-int main()
+// 생성자들
+Stock::Stock() // 디폴트 생성자
 {
-    std::cout << "Hello World!\n";
+	company = "no name";
+	shares = 0;
+	share_val = 0.0;
+	total_val = 0.0;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+Stock::Stock(const std::string &co, long n, double pr)
+{
+	company = co;
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	if (n < 0)
+	{
+		std::cout << "주식 수는 음수가 될 수 없으므로, " << company << " shares를 0으로 설정합니다.\n";
+		shares = 0;
+	}
+	else
+	{
+		shares = n;
+	}
+	share_val = pr;
+	set_tot();
+}
+
+// 클래스 파괴자
+Stock::~Stock() // 말 없는 클래스 파괴자
+{
+
+}
+
+// 다른 메서드들
+void Stock::buy(long num, double price)
+{
+	if (num < 0)
+	{
+		std::cout << "매입 주식 수는 음수가 될 수 없으므로, " << "거래가 취소되었습니다.\n";
+	}
+	else
+	{
+		shares += num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::sell(long num, double price)
+{
+	using std::cout;
+	if (num < 0)
+	{
+		cout << "매도 주식 수는 음수가 될 수 없으므로, " << "거래가 취소되었습니다.\n";
+	}
+	else if (num > shares)
+	{
+		cout << "보유 주식보다 많은 주식을 매도할 수 없으므로, " << "거래가 취소되었습니다.\n";
+	}
+	else
+	{
+		shares -= num;
+		share_val = price;
+		set_tot();
+	}
+}
+
+void Stock::update(double price)
+{
+	share_val = price;
+	set_tot();
+}
+
+void Stock::show() const
+{
+	using std::cout;
+	using std::ios_base;
+	// 포맷을 #.###에 세팅한다.
+	ios_base::fmtflags orig = cout.setf(ios_base::fixed, ios_base::floatfield);
+	std::streamsize prec = cout.precision(3);
+
+	cout << "회사명 : " << company << " 주식 수 : " << shares << '\n';
+	cout << " 주가 : $" << share_val;
+
+	// 포맷을 #.##에 세팅한다.
+	cout.precision(2);
+	cout << " 주식 총 가치 : $" << total_val << '\n';
+
+	// 원본 포맷을 저장한다.
+	cout.setf(orig, ios_base::floatfield);
+	cout.precision(prec);
+}
+
+const Stock & Stock::topval(const Stock & s) const
+{
+	if (s.total_val > total_val)
+	{
+		return s;
+	}
+	else
+	{
+		return *this;
+	}
+}
