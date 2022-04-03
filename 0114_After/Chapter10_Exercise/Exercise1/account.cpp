edfc05036153b636b7d69445e19963b55768874f
fanjaekim <hjjang45@gmail.com>
2022-04-03 23:12:38 +0900

    C++ 기초 플러스 Chapter 10 - 연습문제 1

---
+++ 0114_After/Chapter10_Exercise/Exercise1/account.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<cctype>
+#include"account.h"
+
+BankAccount::BankAccount(const char* client, const char* num, double bal)
+{
+	strcpy(name, client);
+	strcpy(acctnum, num);
+	balance = bal;
+}
+void BankAccount::show(void) const
+{
+	std::cout << "이름 : " << name << "\n";
+	std::cout << "계좌 : " << acctnum << "\n";
+	std::cout << "잔액 : " << balance << "\n";
+}
+void BankAccount::deposit(double cash)
+{
+	balance += cash;
+}
+void BankAccount::withdraw(double cash)
+{
+	if ((balance - cash) < 0)
+	{
+		std::cout << "출금할 금액이 잔액보다 많습니다.\n";
+	}
+	else
+	{
+		balance -= cash;
+	}
+}
